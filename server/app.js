e955a13374a923db91db363a98a422640d6479fe
Yash Pandey (YP) <yash.btech.cs19@iiitranchi.ac.in>
2021-12-25 22:57:38 +0530

    Initialised project :rocket:
    This is the initial commit of the project

---
+++ server/app.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URL, async (err) => {
+  if (err)
+    throw err;
+  console.log("Connected to MongoDB!");
+});
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
