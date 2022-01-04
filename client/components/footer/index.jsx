d22e74a350c6955f8947f9a3a8749344802b96d1
abhishekrathi16 <rathiabhishek53@gmail.com>
2022-01-04 18:36:41 +0530

    footer added

--- client/components/footer/index.jsx
+++ client/components/footer/index.jsx
@@ -1,29 +1,171 @@
-import { Box, Link, Typography } from '@mui/material'
-import React from 'react'
+import { div, Link, Typography, useMediaQuery, useTheme } from "@mui/material";
+import React from "react";
+import styles from "../../styles/Footer.module.css";
+import EmailIcon from "@mui/icons-material/Email";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+// import MobDesign from "./MobDesign";
+// import LaptopDesign from "./LaptopDesign";
+// import TabDesign from "./TabDesign";
 
 export default function Footer() {
-    return (
-        <Box sx={{ padding: '0.5rem' }}>
-            <Typography variant='h5'>Footer</Typography>
-            A property of {' '}
+  const style = {
+    color: "white",
+    backgroundColor: "#25477A",
+  };
+
+  return (
+    <div style={{ backgroundColor: "#25477A", marginTop: "30rem" }} className={styles.wide}>
+      <div
+        style={{
+          padding: "0.5rem",
+          display: "flex",
+          flexDirection: "row",
+          backgroundColor: "#25477A",
+          height: "26.3rem",
+          fontFamily: "DM Sans",
+          textAlign: "left",
+          justifyContent: "center",
+          paddingTop: "40px",
+        }}
+        className={styles.wide}
+      >
+        <div className={styles.padding}>
+          <div className={styles.heading}>Quick Access</div>
+          <div className={styles.class}>
             <Link
-                underline='none'
-                href="https://iiitranchi.ac.in"
-                target="_blank"
-                rel="noopener noreferrer"
+              underline="none"
+              href="#"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: "white" }}
             >
-                IIIT Ranchi
+              B.Tech Course
             </Link>
-            <br />
-            Developed by{' '}
+          </div>
+          <div className={styles.class}>
             <Link
-                underline='none'
-                href="https://houseofgeeks.netlify.app"
-                target="_blank"
-                rel="noopener noreferrer"
+              underline="none"
+              href="#"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: "white" }}
             >
-                House of Geeks
+              M.Tech Course
             </Link>
-        </Box>
-    )
+          </div>
+          <div className={styles.class}>
+            <Link
+              underline="none"
+              href="#"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: "white" }}
+            >
+              Our Alumni
+            </Link>
+          </div>
+          <div className={styles.class}>
+            <Link
+              underline="none"
+              href="#"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: "white" }}
+            >
+              Photo Gallery
+            </Link>
+          </div>
+          <div className={styles.class}>
+            <Link
+              underline="none"
+              href="#"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: "white" }}
+            >
+              Visitors
+            </Link>
+          </div>
+        </div>
+        <div className={styles.padding}>
+          <div className={styles.heading}>Students Corner</div>
+          <div className={styles.class}>
+            <Link
+              underline="none"
+              href="#"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: "white" }}
+            >
+              Youtube Channel
+            </Link>
+          </div>
+          <div className={styles.class}>
+            <Link
+              underline="none"
+              href="#"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: "white" }}
+            >
+              Online Resources
+            </Link>
+          </div>
+          <div className={styles.class}>
+            <Link
+              underline="none"
+              href="#"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: "white" }}
+            >
+              Notifications
+            </Link>
+          </div>
+        </div>
+        <div className={styles.padding}>
+          <div className={styles.heading} style={{marginLeft: "37px"}}>Get In Touch</div>
+          <div className={styles.class}>
+            <EmailIcon style={{marginRight: "15px"}}/>
+            <Link
+              underline="none"
+              href="#"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: "white" }}
+            >
+              tap@iiitranchi.ac.in
+            </Link>
+          </div>
+          <div className={styles.class}>
+            <EmailIcon style={{marginRight: "15px"}} />
+            <Link
+              underline="none"
+              href="#"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: "white" }}
+            >
+              tpo@iiitranchi.ac.in
+            </Link>
+          </div>
+          <div className={styles.class}>
+            <LinkedInIcon style={{marginRight: "15px"}}/>
+            <Link
+              underline="none"
+              href="#"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: "white" }}
+            >
+              linkedin/in/tapcell_iiitranchi
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className={styles.copyright}>
+        © Indian Institute of Information Technology Ranchi 2022
+      </div>
+    </div>
+  )
 }
