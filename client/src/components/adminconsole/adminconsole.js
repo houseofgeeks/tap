28fdf55393a79d50c23f0a104d9bcb23e4b781b4
Anshaj Khare <anshaj13.ugcs20@iiitranchi.in>
2022-03-23 23:45:55 +0530

    Added Admin Console Frontend

---
+++ client/src/components/adminconsole/adminconsole.js
@@ -0,0 +1,27 @@
+import './adminconsole.css';
+
+function App() {
+  return (
+  <>
+    <div className="header">
+      <h1>Admin Login</h1>
+    <form>
+    <div className="container">
+         <label htmlFor="username" className="labeltext">
+           Username
+         </label>
+         <input name="username" id="username" type="text" >
+         </input>
+         <label htmlFor="password" className="labeltext">
+           Password
+         </label>
+         <input name="password" id="password" type="password"></input>
+         <button type="submit">Login</button>
+    </div>
+    </form>
+    </div>
+    </>
+  );
+}
+
+export default App;
