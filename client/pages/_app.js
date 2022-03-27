452a286cf15180ebc3d4368e2f99c81860e09a10
LokeshReddyMeda <lokeshreddy.meda@gmail.com>
2022-03-27 18:58:02 +0530

    added landing img

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,6 @@
 // styling
 import '../styles/globals.css'
+import '../styles/image.css'
 
 // theming
 import { ThemeProvider } from '@mui/material/styles';
@@ -7,10 +8,11 @@ import theme from '../theme/theme';
 
 // views
 import { Footer, Navbar } from '../components';
-
+import Landingimage from '../src/components/Landingimage';
 function MyApp({ Component, pageProps }) {
     return <ThemeProvider theme={theme}>
         <Navbar />
+        <Landingimage />
         <Component {...pageProps} />
         <Footer />
     </ThemeProvider>
---
+++ client/src/components/Landingimage.js
@@ -0,0 +1,5 @@
+export default function Landingimage(){
+    return <div className="image">
+     <img src="/landingimg.jpg" alt="landingimage" />
+    </div>
+}
