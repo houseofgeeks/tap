94c7d48bb9f480f9a2331bbf0773f6b774f50bb8
Anshaj Khare <anshaj13.ugcs20@iiitranchi.in>
2022-04-17 23:34:33 +0530

    added some files

--- client/views/admin/index.js
+++ client/views/admin/index.js
@@ -1,19 +1,37 @@
+import "../admin/adminstyle.css";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { loginUser } from "./UserActions";
+
+
+
 export default function Admin() {
+
+  const [username, setusername] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+
+
+  const loginHandler = (e) => {
+    e.preventDefault();
+
+    dispatch(loginUser(username, password));
+  };
     return (
     <>
       <div className="header">
         <h1>Admin Login</h1>
-      <form>
+      <form onSubmit={loginHandler}>
       <div className="container">
-           <label htmlFor="username" className="labeltext">
+           <label htmlFor="username" className="labeltext" >
              Username
            </label>
-           <input name="username" id="username" type="text" >
+           <input name="username" placeholder="username" id="username" type="text" value={username} onChange={(e) => setusername(e.target.value)} >
            </input>
-           <label htmlFor="password" className="labeltext">
+           <label htmlFor="password"  className="labeltext">
              Password
            </label>
-           <input name="password" id="password" type="password"></input>
+           <input name="password" type="password" id="password" placeholder="password" required value={password} onChange={(e) => setPassword(e.target.value)}></input>
            <button type="submit">Login</button>
       </div>
       </form>
